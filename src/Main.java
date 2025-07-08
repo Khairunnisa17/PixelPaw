79035c3059c35b284996184a5d294794cf720463
nurhijrxh <2024917611@student.uitm.edu.my>
2025-07-08 22:36:58 +0800

    edit frame size

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public static void showPetSelection() {
         JFrame frame = new JFrame("Choose Your Pet");
-        frame.setSize(400, 300);
+        frame.setSize(1300, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(1, 3));
 
